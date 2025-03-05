9980b3a0cf0bcea8603ad2095b17e86d2aa49cf4
JUAN ESTEBAN VELASCO BOHORQUEZ <juanvb@MacBook-Pro-de-JUAN.local>
2025-03-05 08:59:17 -0500

    termina el lab

--- Piedra-Papel-Tijera/index.js
+++ Piedra-Papel-Tijera/index.js
@@ -1,16 +1,46 @@
 const express = require("express");
-const path = require("path");
+const cors = require("cors");
 
 const app = express();
-
 app.use(express.json());
-app.use("/app1", express.static(path.join(__dirname, "app1")));
-app.use("/app2", express.static(path.join(__dirname, "app2")));
+app.use(cors());
 
-let users = [];
+const db = {
+    players: [],
+};
 
-app.get("/users", (req, res) => {
-  res.send(users);
+function determineWinner(player1, player2) {
+    const outcomes = {
+        rock: { rock: "draw", paper: "lose", scissors: "win" },
+        paper: { rock: "win", paper: "draw", scissors: "lose" },
+        scissors: { rock: "lose", paper: "win", scissors: "draw" },
+    };
+
+    const result = outcomes[player1.move][player2.move];
+    return result;
+}
+
+app.post("/user", (request, response) => {
+    const { body } = request;
+    db.players.push(body);
+
+    if (db.players.length === 2) {
+        const result = determineWinner(db.players[0], db.players[1]);
+        db.result = {
+            player1: db.players[0],
+            player2: db.players[1],
+            result,
+        };
+        db.players = [];
+    }
+
+    response.status(201).send(body);
 });
 
-app.listen(5050);
+app.get("/users", (request, response) => {
+    response.send(db.result ? [db.result] : []);
+});
+
+app.listen(5050, () => {
+    console.log(`Server is running on http://localhost:5050`);
+});
